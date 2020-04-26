2c6e6b491cb6413aec97c889f11fc2f4cbb061b9
Dexter Sampson <dexter1.sampson@gmail.com>
2020-04-26 15:03:39 -0400

    show table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,5 +19,17 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    showTable();
     connection.end();
   });
+
+  var showTable = function() {
+      connection.query("SELECT * FROM products", function(err, res){
+        for(var i=0; i<res.length; i++){
+          console.log(res[i].item_id+" || "+res[i].product_name+" || "
+          +res[i].department_name+" || "+res[i].price+" || "
+          +res[i].stock_quanity+"\n");
+        }
+      })
+  } 
+
